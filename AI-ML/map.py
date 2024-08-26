014d100b62a5b2f5855fa9f57087cbc3fdc2dc36
Vaibhav Kokare <vaibhavkokare321@gmail.com>
2024-08-26 09:01:20 +0530

    added Shortest Path Algo

--- AI-ML/map.py
+++ AI-ML/map.py
@@ -1,21 +1,46 @@
 import pandas as pd
 
 
-data = [    
-    ['', 1, 0, 0, 0, 0],
-    [1, '', 1, 0, 1, 0],
-    [0, 1, '', 1, 0, 0],
-    [0, 0, 1, '', 1, 1],
-    [0, 1, 0, 1, '', 1],
-    [0, 0, 0, 1, 1, ''] 
-]
-header = ["Devgad", "Malvan", "Oros", "Kudal", "vengurla", "sawantawadi"]
-df = pd.DataFrame(data, index=header, columns=header )
-print(df)
+header = ["Devgad", "Malvan", "Oros", "Kudal", "Vengurla", "Sawantawadi"]
+map = {
+    "Devgad": [ "Malvan" ],
+    "Malvan": [ "Devgad", "Oros", "Vengurla" ],
+    "Oros": [ "Malvan", "Kudal"],
+    "Kudal": [ "Oros", "Vengurla", "Sawantawadi" ],
+    "Vengurla": [ "Malvan", "Kudal", "Sawantawadi" ],
+    "Sawantawadi": [ "Kudal", "Vengurla" ],
+}
+print(map)
 
 
-def brute_force(df, currect, destination):
-    ...
+
+def brute_force(map, current, destination):
+
+    route = [current]
+    visited_places = [current]
+    search_stack = []
+
+    while current != destination:
+        print(' -> '.join(route))
+        if destination in map[current]:
+            route.append(destination)
+            print("++")
+            break
+        for place in map[current]:
+            if place not in visited_places:
+                print("-", place)
+                search_stack.append(place)
+        
+        print("search next:", search_stack)
+        print("visited_places: ", visited_places, "\n")
+
+        current = search_stack.pop(0)
+        visited_places.append(current)
+
+        route.append(current)
+    
+    print(' -> '.join(route))
+
 
 
 print("\nselect the currect place and destination: ")
@@ -23,18 +48,21 @@ for count, place in enumerate(header):
     print(f"{count}: {place}")
 
 while True:
-    current = int(input("Enter the current cord: "))
+    current = 0 #(input("Enter the current cord: "))
     if current < len(header):
+        current = header[current]
         break
     print("Please, Try again...")
 
 while True:
-    destination = int(input("Enter the destination cord: "))
+    destination = 5 #int(input("Enter the destination cord: "))
     if destination < len(header):
+        destination = header[destination]
         break
     print("Please, Try again...")
 
-print(f"Searching a route from {header[current]} to {header[destination]}")
+print(f"Searching a route from {current} to {destination}")
 
 
-brute_force(df, current, destination)
+
+brute_force(map, current, destination)
